876826033e6464767b6d1eac51b4394dcc00e32b
Ryan Spletzer <ryan.spletzer@autodesk.com>
2023-08-12 19:23:57 -0400

    Update fish config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,17 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export GPG_TTY=$(tty)
+if test "$TERM_PROGRAM" != "Apple_Terminal"
+    oh-my-posh init fish --config ~/.oh-my-posh/themes/mytheme.json | source
+end
+
+alias cls=clear
+alias openremote='open $(git remote get-url origin)'
+
+chruby ruby-3.1.3
+
+alias pwsh='pwsh -NoLogo'
+alias finder='open -a finder'
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
